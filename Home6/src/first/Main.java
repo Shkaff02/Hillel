c8c2b670a4072ab341b917cd83ec0c406c8b338e
Shkaff02 <ksurygin5@gmail.com>
2021-11-04 10:44:27 +0200

    Home6

---
+++ Home6/src/first/Main.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getAreaValue(-1, 100));
+    }
+
+    public static  int getAreaValue(int x, int y){
+        if (x < 0 || y < 0) throw new IllegalArgumentException("value of 'x' or 'y' is negative");
+        return x*y;
+    }
+}
