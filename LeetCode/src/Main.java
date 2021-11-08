3b2aef31070f515fb7f213d276945a98f1d407c9
Shkaff02 <78539412+Shkaff02@users.noreply.github.com>
2021-11-08 14:23:38 +0200

    Delete LeetCode directory

--- LeetCode/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Some leetcode algorithm tasks solution!");
-        System.out.println("Remove Duplicates");
-    }
-}
