1cb97602c14bfedbc001f01c2720060b24660af5
Shkaff02 <ksurygin5@gmail.com>
2021-11-08 14:17:23 +0200

    "Remove Duplicates"

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Some leetcode algorithm tasks solution!");
+        System.out.println("Remove Duplicates");
     }
 }
